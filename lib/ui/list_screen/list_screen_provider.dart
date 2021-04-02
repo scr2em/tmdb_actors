68583cddc18ff3ec01616ccbf9f4162b79bb8348
Akram Mohamed <akram.mohamed0@hotmail.com>
2021-04-03 01:30:58 +0200

    actor list provider added

---
+++ lib/ui/list_screen/list_screen_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:iti_actors/models/Actor.dart';
+import 'package:iti_actors/repositories/actor-repo.dart';
+
+class ActorsProvider extends ChangeNotifier {
+  List<Actor> actors;
+  ActorRepository _actorRepository = ActorRepository();
+
+  ActorsProvider() {
+    getActors();
+  }
+
+  void getActors() {
+    _actorRepository.fetchActors().then((newActors) {
+      actors = newActors;
+      return actors;
+    });
+  }
+}
